fe2cca5364fb29737ca24b58c98b0a93dcf88ae2
Luis Fernando <luisfmoraes.r@gmail.com>
2019-08-08 14:45:45 -0300

    back end da aplicacao

---
+++ src/Controllers/LikeController.js
@@ -0,0 +1,29 @@
+const Dev = require('../Models/Dev');
+
+module.exports = {
+    async store(req, res) {
+        console.log(req.params.devId);
+        console.log(req.headers.user);
+
+        const { user } = req.headers;
+        const { devId } = req.params;
+
+
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
+
+        if (!targetDev) {
+            return res.status(400).json({ error: 'Dev nao existe' });
+        }
+
+        if (targetDev.likes.includes(loggedDev._id)) {
+            console.log('Deu Match');
+        }
+
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+
+        return res.json(loggedDev);
+    }
+};
